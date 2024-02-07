ec7a1d12a2bb6a8545ca14d15a482ab6849edeeb
william <jeremiah.wsebastian@gmail.com>
2024-02-08 00:53:18 +0700

    contactPage finished and fixed picture in landingPage

--- src/pages/contactPage.jsx
+++ src/pages/contactPage.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React from 'react';
+import styles from './css/contactPage.module.css'
 
 const contactPage = () => {
   return (
-    <div>
-      <h1>contacts</h1>
-    </div>
+    <>
+      <div className= {styles.contactTitle}>
+        <div className= {styles.title1}>
+          <h1>Con</h1>
+        </div>
+        <div className= {styles.title2}>
+          <h1>tact</h1>
+        </div>
+      </div>
+
+      <div className= {styles.contactMethods}>
+        <div className= {styles.phoneContainer}>
+          <img src = "../pictures/phoneIcon.png"/>
+          <div className= {styles.phoneNumber}>
+            <h2>+62 813 1658 9191</h2>
+          </div>
+          <div className= {styles.dateContact}>
+            <p>Monday - Friday 9AM - 5PM</p>
+          </div>
+        </div>
+
+        <div className= {styles.houseContainer}>
+          <img src = "../pictures/houseIcon.png"/>
+          <div className= {styles.houseLocation}>
+            <h2>Jakarta, Indonesia</h2>
+          </div>
+          <div className= {styles.specificLocation}>
+            <p>South Tangerang, Indonesia, 15310</p>
+          </div>
+        </div>
+
+        <div className= {styles.emailContainer}>
+          <img src = "../pictures/emailLogo.png"/>
+          <div className= {styles.email}>
+            <h2>jeremiah.wsebastian@gmail.com</h2>
+          </div>
+          <div className= {styles.contactMe}>
+            <p>Contact Me Anytime</p>
+          </div>
+        </div>
+      </div>
+    </>
+
+
   )
 }
 
